cd51ec80075d51f8c553a313876c524fdf2467f9
Muhammed Koşan <muhammedkoshan29@gmail.com>
2025-12-06 15:21:32 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,59 +1,6 @@
 #include <iostream>
-#include "clsPerson.h"
-#include "clsBankClient.h"
-#include "clsInputValidate.h"
-#include <iomanip>
-#include "clsUtil.h";
 
-void PrintClientRecordLine(clsBankClient Client)
-{
-
-    cout << "| " << setw(15) << left << Client.AccountNumber();
-    cout << "| " << setw(40) << left << Client.FullName();
-    cout << "| " << setw(20) << left << Client.AccountBalance;
-
-}
-
-void ShowTotalBalances()
-{
-
-    vector <clsBankClient>vClients = clsBankClient::GetClientsList();
-    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    cout << "| " << left << setw(15) << "Account Number";
-    cout << "| " << left << setw(40) << "Client Name";
-    cout << "| " << left << setw(20) << "Balance";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-    if (vClients.size() == 0) {
-
-        cout << "\t\t\t No Clients Available In The System! ";
-
-    }
-    else {
-
-        for (clsBankClient Client : vClients) {
-
-            PrintClientRecordLine(Client);
-            cout << endl;
-
-
-        }
-        cout << "\n_______________________________________________________";
-        cout << "_________________________________________\n" << endl;
-
-        cout << "\n\t\t\t\tTotal Balances = " << clsBankClient::GetTotalBalances() << "\n";
-        cout << "\t\t\t\t( " << clsUtil::NumberToText(clsBankClient::GetTotalBalances()) << ")";
-
-
-    }
-
-}
-
-
-
+#include "clsMainScreen.h"
 
 
 
@@ -61,7 +8,7 @@ void ShowTotalBalances()
 int main()
 
 {
-    ShowTotalBalances();
+    clsMainScreen::ShowMainMenue();
     system("pause>0");
     return 0;
 }
