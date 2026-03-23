1100b627d6e7360c0da0b4a66866cfeeacf31aac
Muhammed Koşan <muhammedkoshan29@gmail.com>
2026-03-23 17:56:41 +0300

    Update clsDepositScreen.h

--- clsDepositScreen.h
+++ clsDepositScreen.h
@@ -1,83 +1,62 @@
 #pragma once
-#include <iostream>
-#include "clsScreen.h";
+#include  <iostream>
+#include "clsScreen.h"
 #include "clsBankClient.h"
+
 #include "clsInputValidate.h"
-
-
+using namespace std;
 class clsDepositScreen : protected clsScreen {
 
 private:
 
-    static void _PrintClient(clsBankClient Client) {
+	static void _PrintClient(clsBankClient Client)
+	{
+		cout << "\nClient Card:";
+		cout << "\n___________________";
+		cout << "\nFirstName   : " << Client.FirstName;
+		cout << "\nLastName    : " << Client.LastName;
+		cout << "\nFull Name   : " << Client.FullName();
+		cout << "\nEmail       : " << Client.Email;
+		cout << "\nPhone       : " << Client.Phone;
+		cout << "\nAcc. Number : " << Client.AccountNumber();
+		cout << "\nPassword    : " << Client.PinCode;
+		cout << "\nBalance     : " << Client.AccountBalance;
+		cout << "\n___________________\n";
 
-        cout << "\nClient Card:";
-        cout << "\n___________________";
-        cout << "\nFirstName   : " << Client.FirstName;
-        cout << "\nLastName    : " << Client.LastName;
-        cout << "\nFull Name   : " << Client.FullName();
-        cout << "\nEmail       : " << Client.Email;
-        cout << "\nPhone       : " << Client.Phone;
-        cout << "\nAcc. Number : " << Client.AccountNumber();
-        cout << "\nPassword    : " << Client.PinCode;
-        cout << "\nBalance     : " << Client.AccountBalance;
-        cout << "\n___________________\n";
-
-    }
-
-	static string _ReadAccountNumber() {
-
-        string AccountNumber = "";
-        cout << "\nPlease enter AccountNumber? ";
-        cin >> AccountNumber;
-        return AccountNumber;
 	}
 
+public :
 
+	static void ShowDepositScreen() {
 
-public:
+		_DrawScreenHeader("\tDeposit Screen");
+		cout << "Enter Account Number to deposit";
+		string AccountNumber = clsInputValidate::ReadString();
+		while (!clsBankClient::IsClientExist(AccountNumber)) {
+			cout << "No client in this Account Number try another one: ";
+			cin >> AccountNumber;
+		}
+		clsBankClient Client = clsBankClient::Find(AccountNumber);
+		_PrintClient(Client);
+		cout << "How Much you want to deposit: ";
+		int Amount = clsInputValidate::ReadDblNumber();
+		cout << "Are you sure you want to perform this transaction? y/n? ";
+		char Answer = 'n';
+		cin >> Answer;
+		if (Answer == 'y' || Answer == 'Y')
+		{
 
-    static void ShowDepositScreen()
-    {
-        _DrawScreenHeader("\t   Deposit Screen");
-
-        string AccountNumber = _ReadAccountNumber();
-
-
-        while (!clsBankClient::IsClientExist(AccountNumber))
-        {
-            cout << "\nClient with [" << AccountNumber << "] does not exist.\n";
-            AccountNumber = _ReadAccountNumber();
-        }
-
-        clsBankClient Client1 = clsBankClient::Find(AccountNumber);
-        _PrintClient(Client1);
-
-        double Amount = 0;
-        cout << "\nPlease enter deposit amount? ";
-        Amount = clsInputValidate::ReadDblNumber();
-
-        cout << "\nAre you sure you want to perform this transaction? ";
-        char Answer = 'n';
-        cin >> Answer;
-
-        if (Answer == 'Y' || Answer == 'y')
-        {
-            Client1.Deposit(Amount);
-            cout << "\nAmount Deposited Successfully.\n";
-            cout << "\nNew Balance Is: " << Client1.AccountBalance;
-
-        }
-        else
-        {
-            cout << "\nOperation was cancelled.\n";
-        }
-
-    }
+			Client.Deposit(Amount);
+		 
 
 
 
+	}
+		else {
+			cout << "Deposit operation cancelled";
+		}
 
 
+	}
 
 };
