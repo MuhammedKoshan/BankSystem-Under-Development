20cb7bfdc55ec10ed384c0d1d4974e7a5aa136cc
Muhammed Koşan <muhammedkoshan29@gmail.com>
2026-03-23 17:57:02 +0300

    Update clsFindClientScreen.h

--- clsFindClientScreen.h
+++ clsFindClientScreen.h
@@ -1,71 +1,68 @@
 #pragma once
 #include <iostream>
 #include "clsScreen.h"
-#include "clsBankClient.h"
 #include "clsInputValidate.h"
+#include "clsBankClient.h"
 using namespace std;
-
 
 class clsFindClientScreen : protected clsScreen {
 
-
-
 private:
+	static void _PrintClient(clsBankClient Client)
+	{
+		cout << "\nClient Card:";
+		cout << "\n___________________";
+		cout << "\nFirstName   : " << Client.FirstName;
+		cout << "\nLastName    : " << Client.LastName;
+		cout << "\nFull Name   : " << Client.FullName();
+		cout << "\nEmail       : " << Client.Email;
+		cout << "\nPhone       : " << Client.Phone;
+		cout << "\nAcc. Number : " << Client.AccountNumber();
+		cout << "\nPassword    : " << Client.PinCode;
+		cout << "\nBalance     : " << Client.AccountBalance;
+		cout << "\n___________________\n";
 
+	}
 
-    static void _PrintClient(clsBankClient Client) {
+public :
 
-        cout << "\nClient Card:";
-        cout << "\n___________________";
-        cout << "\nFirstName   : " << Client.FirstName;
-        cout << "\nLastName    : " << Client.LastName;
-        cout << "\nFull Name   : " << Client.FullName();
-        cout << "\nEmail       : " << Client.Email;
-        cout << "\nPhone       : " << Client.Phone;
-        cout << "\nAcc. Number : " << Client.AccountNumber();
-        cout << "\nPassword    : " << Client.PinCode;
-        cout << "\nBalance     : " << Client.AccountBalance;
-        cout << "\n___________________\n";
+	static void ShowFindClient() {
+		if (!CheckAccessRights(clsUser::enPermissions::pFindClient)) {
+			return;
+		}
 
-    }
+		string Title = "\t  Find Client Screen";
+		string SubTitle = "";
 
+		_DrawScreenHeader(Title, SubTitle);
+		string AccountNumber = "";
+		cout << "Enter Account Number: ";
+		cin >> AccountNumber;
+		while (!clsBankClient::IsClientExist(AccountNumber)) {
+			cout << "account number dores exist try another one: ";
+			cin >> AccountNumber;
+		}
+		vector<clsBankClient>vClients = clsBankClient::GetClientsList();
 
+		for (clsBankClient C : vClients) {
+			if (AccountNumber == C.AccountNumber()) {
 
-
-public:
-
-
-    static void ShowUpdateClientScreen()
-    {
-
-        _DrawScreenHeader("\tFind Client Screen");
-
-        string AccountNumber = "";
-
-        cout << "\nPlease Enter client Account Number: ";
-        AccountNumber = clsInputValidate::ReadString();
-
-        while (!clsBankClient::IsClientExist(AccountNumber))
-        {
-            cout << "\nAccount number is not found, choose another one: ";
-            AccountNumber = clsInputValidate::ReadString();
-        }
-
-        clsBankClient Client1 = clsBankClient::Find(AccountNumber);
-        if (!Client1.IsEmpty()) {
-            cout << "\nClient Found :-)\n";
-
-        }
-        else {
-            cout << "\n Client Was not Found :-(\n";
-        }
-        _PrintClient(Client1);
+				cout << "Yes We Find It\n ";
+				_PrintClient(C);
+				return;
 
 
 
+			}
+
+		}
 
 
-    }
+
+
+
+	}
+
 
 
 
