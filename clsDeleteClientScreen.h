57d1650df345ca7f79ec35dd279296870170ed3a
Muhammed Koşan <muhammedkoshan29@gmail.com>
2026-03-23 17:56:10 +0300

    Update clsDeleteClientScreen.h

--- clsDeleteClientScreen.h
+++ clsDeleteClientScreen.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <iostream>
 #include "clsScreen.h"
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
 using namespace std;
 
-
 class clsDeleteClientScreen : protected clsScreen {
-
 private:
-    static void _PrintClient(clsBankClient Client) {
-
+    static void _PrintClient(clsBankClient Client)
+    {
         cout << "\nClient Card:";
         cout << "\n___________________";
         cout << "\nFirstName   : " << Client.FirstName;
@@ -22,48 +22,50 @@ private:
         cout << "\n___________________\n";
 
     }
+public:
 
-public :
+   static void ShowDeleteClient() {
+       if (!CheckAccessRights(clsUser::enPermissions::pDeleteClient) ){
+           return;
+       }
 
-    static void ShowDeleteClientScreen()
-    {
+       string Title = "\t  Delete Client Screen";
+       string SubTitle = "";
 
-        _DrawScreenHeader("\tDelete Client Screen");
+       _DrawScreenHeader(Title, SubTitle);
+        cout << "Please Enter the account Number: ";
 
-        string AccountNumber = "";
+        string AccountNumber = clsInputValidate::ReadString();
+        while (!(clsBankClient::IsClientExist(AccountNumber))) {
 
-        cout << "\nPlease Enter Account Number: ";
-        AccountNumber = clsInputValidate::ReadString();
-        while (!clsBankClient::IsClientExist(AccountNumber))
-        {
-            cout << "\nAccount number is not found, choose another one: ";
+            cout << "This Client AccountNumber is not exist Try another one: ";
             AccountNumber = clsInputValidate::ReadString();
         }
 
-        clsBankClient Client1 = clsBankClient::Find(AccountNumber);
-        _PrintClient(Client1);
+        clsBankClient Client = clsBankClient::Find(AccountNumber);
 
-        cout << "\nAre you sure you want to delete this client y/n? ";
-
-        char Answer = 'n';
+        _PrintClient(Client);
+        char Answer;
+        cout << "Are you sure you want to delete this client? \n";
         cin >> Answer;
+        if (Answer == 'Y' || Answer == 'y') {
+            if (Client.Delete()) {
 
-        if (Answer == 'y' || Answer == 'Y')
-        {
+                cout << "Client Deleted Successfully";
+                _PrintClient(Client);
 
 
-            if (Client1.Delete())
-            {
-                cout << "\nClient Deleted Successfully :-)\n";
-                _PrintClient(Client1);
+
             }
-            else
-            {
-                cout << "\nError Client Was not Deleted\n";
+            else {
+                cout << "Error Client Wasnt deleted";
             }
         }
-    }
 
+
+
+
+    }
 
 
 
